0b19072317f6eaa837ef4e1f2a20b73a640bfa84
Manuel Silva <manuel@mjs.me>
2025-03-03 13:03:17 +1100

    adds testing framework

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className={"grid place-content-center h-screen"}>
-      <h1 className={"text-4xl animate-pulse"}>Welcome to NextJS starter</h1>
+      <h1 className={"text-4xl animate-pulse"} role={"welcome-text"}>Welcome to NextJS starter</h1>
     </div>
   );
 }
